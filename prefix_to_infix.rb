09f2e8952bdf01e88c055dc14b978a3d54de9f64
NanthiniDL <nanthini.jeyamani@drylogics.com>
2024-03-17 23:23:08 +0530

    programs

--- prefix_to_infix.rb
+++ prefix_to_infix.rb
@@ -1,16 +1,22 @@
 def infix(input)
-    operator = []
-    rev_operator = []
-    operand = [] 
-    postfix = input.split('')
-    symbol = ["+", "-", "*", "/"]
-  
-    postfix.each do |item|
-      if symbol.include?(item)
-        operator << item
-      else
-        operand << item  
-      end
+  operator = []
+  rev_operator = []
+  operand = []
+  postfix = input.split('')
+  symbol = ["+", "-", "/"]
+
+  postfix.each do |item|
+    if symbol.include?(item)
+      operator << item
+    else
+      operand << item
     end
+    puts operand
+    puts operator
+  end
+  infix_expres = operand[0] + operator[2] + operand[1] + operator[1] + operand[2] + operator[0] + operand[3]
+  puts infix_expres
+
 end
-infix(abcd++-/)
+infix("abcd+-/")
+#a/(b-(c+d))
